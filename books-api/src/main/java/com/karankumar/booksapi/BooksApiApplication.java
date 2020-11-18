fb5ba47f13223bf54fc9f84103439daf0c02ba5c
Karan <karan@karankumar.com>
2020-11-18 18:19:47 +0000

    add empty Spring project

---
+++ books-api/src/main/java/com/karankumar/booksapi/BooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.karankumar.booksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApiApplication.class, args);
+	}
+
+}
