5b72d0c33502cd4c3d38696dca82e4f69351a15b
Chetan Chilhate <chilhatechetan04@gmail.com>
2020-11-24 22:53:48 +0530

    removed unused class

--- src/main/java/com/karankumar/booksapi/BooksApiApplication.java
+++ src/main/java/com/karankumar/booksapi/BooksApiApplication.java
@@ -27,8 +27,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 public class BooksApiApplication {
     public static void main(String[] args) {
