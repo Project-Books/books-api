75ccd9d7dcabd1194914acc6042e176ad051a3f2
Vivek Kaul <keviv.rocks@gmail.com>
2021-03-15 00:33:48 +0530

    Update BookSeriesMapping entity - Unique constraint, Relationship fields are Not Null, minor corrections

--- src/main/java/com/karankumar/booksapi/model/BookSeriesMapping.java
+++ src/main/java/com/karankumar/booksapi/model/BookSeriesMapping.java
@@ -21,11 +21,12 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-
 @Table(
         name="Book_To_BookSeries_Mapping",
-        uniqueConstraints =
-                @UniqueConstraint( columnNames = {"book_id","book_series_id","serialNumber"} )
+        uniqueConstraints = {
+                @UniqueConstraint( name = "UK_bookSeriesId_bookId", columnNames = {"book_series_id","book_id"} ),
+                @UniqueConstraint( name = "UK_bookSeriesId_serialNumber", columnNames = {"book_series_id","serial_number"} )
+        }
 )
 
 public class BookSeriesMapping {
@@ -33,18 +34,18 @@ public class BookSeriesMapping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
-    private long id;
+    private Long id;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private BookSeries bookSeries;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private Book book;
 
-    @Column(nullable = false, name = "serialNumber")
-    private int serialNumber;
+    @Column(nullable = false, name="serial_number")
+    private Integer serialNumber;
 
-    public BookSeriesMapping(@NonNull BookSeries bookSeries, @NonNull Book bookInSeries, @NonNull int serialNumber) {
+    public BookSeriesMapping(@NonNull BookSeries bookSeries, @NonNull Book bookInSeries, @NonNull Integer serialNumber) {
         this.bookSeries = bookSeries;
         this.book = bookInSeries;
         this.serialNumber = serialNumber;
