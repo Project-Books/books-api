6bd042c45d06c1c615ecd8ab7905fcbe9732d5f1
Karan <karan@karankumar.com>
2021-03-25 14:21:24 +0000

    Add missing copyright notice

--- src/test/java/com/karankumar/booksapi/repository/BookSeriesMappingRepositoryTest.java
+++ src/test/java/com/karankumar/booksapi/repository/BookSeriesMappingRepositoryTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2021  Karan Kumar
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.karankumar.booksapi.repository;
 
 import com.karankumar.booksapi.annotations.DataJpaIntegrationTest;
@@ -21,7 +36,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaIntegrationTest
 @DisplayName("BookSeriesMappingRepository should")
-public class BookSeriesMappingRepositoryTest {
+class BookSeriesMappingRepositoryTest {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
@@ -29,8 +44,10 @@ public class BookSeriesMappingRepositoryTest {
     private final BookSeriesMappingRepository bookSeriesMappingRepository;
 
     @Autowired
-    BookSeriesMappingRepositoryTest(BookRepository bookRepository, AuthorRepository authorRepository,
-                                    BookSeriesRepository bookSeriesRepository, BookSeriesMappingRepository bookSeriesMappingRepository){
+    BookSeriesMappingRepositoryTest(BookRepository bookRepository,
+                                    AuthorRepository authorRepository,
+                                    BookSeriesRepository bookSeriesRepository,
+                                    BookSeriesMappingRepository bookSeriesMappingRepository) {
 
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
@@ -48,8 +65,7 @@ public class BookSeriesMappingRepositoryTest {
 
     @Test
     @DisplayName("return all books in a series.")
-    void fetchAllBooksInBookSeries(){
-
+    void fetchAllBooksInBookSeries() {
         BookSeries bs = createBookSeriesMapping();
         List<Book> assertion = bookSeriesMappingRepository.getAllBooksByBookSeries(bs);
 
@@ -60,7 +76,7 @@ public class BookSeriesMappingRepositoryTest {
         assertThat(assertion).hasSize(bookList.size()).hasSameElementsAs(bookList);
     }
 
-    private BookSeries createBookSeriesMapping(){
+    private BookSeries createBookSeriesMapping() {
 
         Author author = new Author("J.K. Rowling");
         author.setAbout("A fantastic author");
