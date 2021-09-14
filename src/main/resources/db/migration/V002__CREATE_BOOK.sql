8a2647889b8483bf4dcd4b949bda3e669c739444
Karan <karan@karankumar.com>
2021-09-14 10:20:32 +0100

    Fix formatting

--- src/main/resources/db/migration/V002__CREATE_BOOK.sql
+++ src/main/resources/db/migration/V002__CREATE_BOOK.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS book (
     id                  SERIAL       NOT NULL PRIMARY KEY,
     title               VARCHAR(255) NOT NULL,
-    languageName            INTEGER      NOT NULL,
+    languageName        INTEGER      NOT NULL,
     isbn13              VARCHAR(255),
     genre               INTEGER      NOT NULL,
     year_of_publication INTEGER,
     blurb               VARCHAR(255) NOT NULL,
     published_by        INTEGER,
-    formatType              INTEGER      NOT NULL
-)
+    formatType          INTEGER      NOT NULL
+);
