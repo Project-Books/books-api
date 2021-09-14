bb9a16f7838e6f25bf150d5eecbe96dd84b15f2a
Karan <karan@karankumar.com>
2021-09-14 09:42:04 +0100

    Fix wrong column reference

--- src/main/java/com/karankumar/booksapi/model/cover/BookCover.java
+++ src/main/java/com/karankumar/booksapi/model/cover/BookCover.java
@@ -12,7 +12,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -45,7 +44,6 @@ public class BookCover {
     private CoverFileType largeFileType;
 
     @ManyToOne
-    @JoinColumn(name = "id")
     private Book book;
 
     @Transient
