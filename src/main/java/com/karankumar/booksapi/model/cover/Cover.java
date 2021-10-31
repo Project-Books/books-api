c4ba158c322a3041a9677f6b1b4df08c054ceea1
john hanna <jhanna60@hotmail.com>
2021-10-31 13:27:26 +0000

    Adding back in specific imports to adhere to style guidelines (IntelliJ inserted a wildcard automatically for me last time)

--- src/main/java/com/karankumar/booksapi/model/cover/Cover.java
+++ src/main/java/com/karankumar/booksapi/model/cover/Cover.java
@@ -7,7 +7,16 @@ import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.FetchType;
 import java.util.Objects;
 
 /**
