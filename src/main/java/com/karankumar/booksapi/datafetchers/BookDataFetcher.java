f9131e0f725a176576bda2f75158570b73fcc990
Vladimir Parfenov1 <vladimir_parfenov1@epam.com>
2022-10-03 19:34:52 +0400

    indentation

--- src/main/java/com/karankumar/booksapi/datafetchers/BookDataFetcher.java
+++ src/main/java/com/karankumar/booksapi/datafetchers/BookDataFetcher.java
@@ -27,9 +27,9 @@ import java.util.List;
 
 @DgsComponent
 public class BookDataFetcher {
-        private final BookService bookService;
+    private final BookService bookService;
 
-    public BookDataFetcher(BookService bookService) {
+     public BookDataFetcher(BookService bookService) {
         this.bookService = bookService;
     }
 
